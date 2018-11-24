c555743fd4efee26339e9ae0d459213ec28b9aaf
Cheng Vue <cvu3@ucsc.edu>
2018-11-24 10:38:49 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,11 +4,19 @@ class School
     @name = name
   end
 
+  def add_to_roster(student, grade)
+    if @roster == {}
+      @roster = {grade => student}
+
+    end
+
+  end
+
   def roster
     @roster = {}
   end
 
   def add_student(student, grade)
-    @roster = {grade => student}
+    add_to_roster(student, grade)
   end
 end
